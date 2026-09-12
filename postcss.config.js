4abf7c13b43ee300aaddb9b35b971ef7e7b57a85
Matthijs Kuiper <info@matthijskuiper.nl>
2026-09-12 16:13:56 +0200

    Update dependencies and align CSS browser targets

--- postcss.config.js
+++ postcss.config.js
@@ -3,8 +3,7 @@ module.exports = {
   plugins: {
     "postcss-import": {},
     "postcss-preset-env": {
-      stage: 0,
-      browsers: "last 2 versions"
+      stage: 0
     },
     cssnano: {}
   }
