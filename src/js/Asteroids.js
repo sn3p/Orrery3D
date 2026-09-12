f8f5fb97d2f4c8a7fd7a0d2fc0b88127fc35c93d
Matthijs Kuiper <info@matthijskuiper.nl>
2026-09-12 22:39:48 +0200

    Remove redundant catalogue ownership comment

--- src/js/Asteroids.js
+++ src/js/Asteroids.js
@@ -124,8 +124,6 @@ export default class Asteroids extends THREE.Points {
     material.customProgramCacheKey = () => "asteroid-orbits-r186-v1";
     super(geometry, material);
     this.name = "Asteroids";
-    // Packed arrays own all runtime state, including graphics recovery. Do not
-    // retain the parsed catalogue objects once preparation has finished.
     this.discoveryDates = packed.dates;
     this.phases = packed.phases;
     this.epoch = jed;
