eb1a4fc7d105566500a2d06a319810b8d370bdd7
Matthijs Kuiper <info@matthijskuiper.nl>
2026-09-12 18:06:09 +0200

    Add browser regression coverage for playback and GPU lifecycle

---
+++ tests/browser.js
@@ -0,0 +1,10 @@
+// Test entry imports the real application boot path, including catalogue fetch.
+import { orrery, ready } from "../src";
+import Orrery3D from "../src/js/Orrery3D";
+import Asteroids, { REBASE_DAYS, REFERENCE_JED } from "../src/js/Asteroids";
+import Orbit from "../src/js/Orbit";
+import * as THREE from "three";
+
+window.testReady = ready.then(() => {
+  window.test = { app: orrery, catalog: orrery?.asteroidData, Orrery3D, Asteroids, Orbit, THREE, REBASE_DAYS, REFERENCE_JED };
+});
